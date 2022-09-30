09e746b1fc109c6a74fcd8e8ff7e78808e1f0ed4
Lucas Alves <as.lucasalves@gmail.com>
2022-09-30 00:10:44 -0300

    projeto iniciado

---
+++ src/theme/colors.js
@@ -0,0 +1,16 @@
+export const colors = {
+  positive: "#2cbe82",
+  negative: "#da2b40",
+  white: "#fff",
+  light: "#f4f4f4",
+  dark: "#1a1f29",
+
+  primary: "#6688ff",
+  secondary: "#ffa246",
+  lightGreen: "#35e185",
+  purple: "#b266ff",
+  lightBlue: "#66d1ff",
+  pink: "#ff66d4",
+  lightRed: "#ff6666",
+  darkRed: "#98031e",
+};
